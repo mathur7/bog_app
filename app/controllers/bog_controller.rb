7a6e09f4bc20a22a0249c54d8f3adb1529fc27c5
mathur7 <mathur7@gmail.com>
2014-04-23 10:08:27 -0700

    got the bonus views working and the edit button on homepage

--- app/controllers/bog_controller.rb
+++ app/controllers/bog_controller.rb
@@ -24,8 +24,8 @@ class BogController < ApplicationController
         creature_id = params[:id]
         creature = Creature.find(creature_id)
         updated_attributes = params.require(:creature).permit(:name, :description)
-        creature.update_attributes(updated_attributes)
-        redirect_to "/posts/#{creature.id}"
+        Creature.update(creature, updated_attributes)
+        redirect_to "/bog/#{creature.id}"
     end
 
 
